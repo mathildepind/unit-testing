10b604ee209bdc8e02496c5eeb14043fc10768a6
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-02-05 16:09:16 +0000

    3 test cases

--- src/index.js
+++ src/index.js
@@ -3,3 +3,64 @@ function add(a, b){
 }
 
 exports.add = add;
+
+
+function longestString(arrayOfStrings) {
+  var output = "";
+
+  arrayOfStrings.forEach(function(word){
+    if (word.length > output.length){
+      output = word;
+    }
+  }
+
+  );
+
+  return output;
+}
+
+exports.longestString = longestString;
+
+function l337(string) {
+  var arrayFromString = string.split('');
+
+  for (let i=0; i<arrayFromString.length; i++){
+    switch(arrayFromString[i]) {
+      case `i`:
+      case 'l':
+        arrayFromString[i] = '1';
+        break;
+      case 'z':
+        arrayFromString[i] = '2';
+        break;
+      case 'e':
+        arrayFromString[i] = '3';
+        break;
+      case 'a':
+        arrayFromString[i] = '4';
+        break;
+      case 's':
+        arrayFromString[i] = '5';
+        break;
+      case 'g':
+        arrayFromString[i] = '6';
+        break;
+      case 't':
+      case 'y':
+        arrayFromString[i] = '7';
+        break;
+      case 'b':
+        arrayFromString[i] = '8';
+        break;
+      case 'q':
+        arrayFromString[i] = '9';
+        break;
+      case 'o':
+        arrayFromString[i] = '0';
+        break;
+    }
+  }
+  return arrayFromString.join('');
+}
+
+exports.l337 = l337;
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,23 @@
-const functions = require('../src/index');
+const functions = require('../src/index.js');
 
 test('Addition', function(){
   const expected = 5;
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+
+test('longestString',function(){
+  const result = functions.longestString(['a', 'bb', 'ccc', 'dddd', 'eeeee']);
+  const expected = 'eeeee';
+  expect(result).toBe(expected);
+})
+
+test("l337", function(){
+  const result1 = functions.l337("izeasgt");
+  const expected1 = "1234567";
+  const result2 = functions.l337("lybqo");
+  const expected2 = "17890";
+  expect(result1).toBe(expected1);
+  expect(result2).toBe(expected2);
+})
